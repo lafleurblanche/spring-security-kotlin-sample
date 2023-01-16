b0d55dfe2265793146c8a24b8e7ba777b87e868d
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-01-17 07:14:02 +0900

    20230117 First Commit

---
+++ src/main/kotlin/net/konohana/sakuya/sample/form/UserRegistrationForm.kt
@@ -0,0 +1,26 @@
+package net.konohana.sakuya.sample.form
+
+import javax.validation.constraints.AssertTrue
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+import org.springframework.validation.annotation.Validated
+
+@Validated
+data class UserRegistrationForm(
+    @field:NotBlank
+    @field:Size(min = 4, max = 20)
+    val username: String? = null,
+
+    @field:NotBlank
+    @field:Size(min = 8, max = 20)
+    val password: String? = null,
+
+    @field:NotBlank
+    @field:Size(min = 8, max = 20)
+    val confirmPassword: String? = null,
+) {
+    val isPasswordValid: @AssertTrue Boolean
+        get() = if (password == null || confirmPassword == null) {
+            false
+        } else password == confirmPassword
+}
