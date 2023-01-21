8bb62a0342110d5258894010e8137f4910a8f6f9
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-01-21 21:50:59 +0900

    20230121 Add KDoc

--- src/main/kotlin/net/konohana/sakuya/sample/controller/AccountController.kt
+++ src/main/kotlin/net/konohana/sakuya/sample/controller/AccountController.kt
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 
+/**
+ * Controller
+ */
 @Controller
 @Validated
 class AccountController {
