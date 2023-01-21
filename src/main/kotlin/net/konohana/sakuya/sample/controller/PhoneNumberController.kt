754dbe6df6c1a9d20f365f047e9cee83d80c46a9
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-01-21 21:51:06 +0900

    20230121 Add KDoc

--- src/main/kotlin/net/konohana/sakuya/sample/controller/PhoneNumberController.kt
+++ src/main/kotlin/net/konohana/sakuya/sample/controller/PhoneNumberController.kt
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * Controller
+ */
 @RestController
 class PhoneNumberController {
 
